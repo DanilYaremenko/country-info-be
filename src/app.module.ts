60639fd8f5625d014191cc85b1fb5f09f00e0dc7
DanilYaremenko <danilyaremenko@gmail.com>
2025-04-03 20:26:57 +0300

    feat: configure global settings and import needed modules in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { CountryModule } from './country/country.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CalendarEventModule } from './calendar-event/calendar-event.module';
+import { CountriesNowClientModule } from './countries-now-client/countries-now-client.module';
+import { NagerClientModule } from './nager-client/nager-client.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: +configService.get<string>('POSTGRES_PORT') || 5432,
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // dev only
+      }),
+    }),
+    UserModule,
+    CountryModule,
+    CalendarEventModule,
+    CountriesNowClientModule,
+    NagerClientModule,
+  ],
 })
 export class AppModule {}
